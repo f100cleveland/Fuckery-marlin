d3f4cd11cf787e077ffa966e545ca96dc66e6f9a
Dustin Rinne <dustin.rinne@gmail.com>
2017-08-16 14:11:20 -0500

    Update build.sh for 6.x tc

--- build.sh
+++ build.sh
@@ -12,19 +12,19 @@ clear
 THREAD="-j26"
 KERNEL="Image"
 DTBIMAGE="dtb"
-export CROSS_COMPILE=${HOME}/tipsy/prebuilts/gcc/linux-x86/aarch64/aarch64-8.0/bin/aarch64-
+export CROSS_COMPILE=${HOME}/roms/pd/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-6.x/bin/aarch64-linux-android-
 DEFCONFIG="fuckery_defconfig"
 
 # Kernel Details
-VER=".pissel-1.1-upstream"
+VER=".pissel-6x-1.14"
 
 # Paths
 KERNEL_DIR=`pwd`
-REPACK_DIR="${HOME}/AnyKernel"
-PATCH_DIR="${HOME}/AnyKernel/patch"
-MODULES_DIR="${HOME}/AnyKernel/modules"
-ZIP_MOVE="${HOME}/fuckery"
-ZIMAGE_DIR="${HOME}/pissel/arch/arm64/boot"
+REPACK_DIR="${HOME}/kernels/AnyKernel"
+PATCH_DIR="${HOME}/kernels/AnyKernel/patch"
+MODULES_DIR="${HOME}/kernels/AnyKernel/modules"
+ZIP_MOVE="${HOME}/kernels/fuckery-out"
+ZIMAGE_DIR="${HOME}/kernels/fuckery/arch/arm64/boot"
 
 # Functions
 function clean_all {
@@ -55,12 +55,12 @@ function make_dtb {
 }
 
 function make_boot {
-		cp -vr $ZIMAGE_DIR/Image.gz-dtb ~/AnyKernel/zImage
+		cp -vr $ZIMAGE_DIR/Image.gz-dtb ~/kernels/AnyKernel/zImage
 }
 
 
 function make_zip {
-		cd ~/AnyKernel
+		cd ~/kernels/AnyKernel
 		zip -r9 `echo $AK_VER`.zip *
 		mv  `echo $AK_VER`.zip $ZIP_MOVE
 		cd $KERNEL_DIR
